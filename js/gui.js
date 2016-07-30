6235812a4512cc59827425df3b285291bfa4bd83
Noël Ströhmer-Lohfink <noel@strongground.de>
2016-07-30 18:26:55 +0200

    added blinking cursor by adding a library for getting caret position and a rather gruesome CSS hack for hiding default caret

--- js/gui.js
+++ js/gui.js
@@ -7,6 +7,7 @@
 		input_form.container = $('div.inputform');
 		input_form.self = $('form[name="form"]');
 		input_form.query_input = $('#queryText');
+		input_form.marker = input_form.self.find('.marker');
 		option_form.container = $('div.optionform');
 		option_form.container_width = $('div.optionform').width();
 		option_form.self = option_form.container.find('form[name="options"]');
@@ -27,10 +28,22 @@
     		})
     	}
 
+    	function blinkingCursor() {
+			caret_position = input_form.query_input.caret('offset');
+			cursor_position = caret_position.left - input_form.marker.width();
+			input_form.marker.css({
+				'left': cursor_position+'px'
+			})
+    	}
+
     	// center the lower option form dynamically because CSS won't work with absolute positioning
     	centerOptionForm();
     	// enable toggling of the option form element
     	enableToggleOptionForm();
+    	// simulate blinking cursor at end of input
+	    input_form.query_input.on('input click', function() {
+	    	blinkingCursor()
+	    });
     };
 
 }( jQuery ));
