ffb60a0e200ec3b0daeb6b28a533c1758345b75e
Noël Ströhmer-Lohfink <noel@strongground.de>
2016-08-02 21:16:03 +0200

    make the linter happy :)

--- js/globals.js
+++ js/globals.js
@@ -1,8 +1,8 @@
 // define globals
-var viewport = new Array();
-viewport['height'] = $(window).height();
-viewport['width'] = $(window).width();
-viewport['center_x'] = viewport['width'] / 2;
-viewport['center_y'] = viewport['height'] / 2;
+var viewport = []
+viewport['height'] = $(window).height()
+viewport['width'] = $(window).width()
+viewport['center_x'] = viewport['width'] / 2
+viewport['center_y'] = viewport['height'] / 2
 
 console.log('global variables initialized')
